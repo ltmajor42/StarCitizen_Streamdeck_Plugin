8785af5158407ab5f9eb413ad113209d2a25b151
ltmajor42 <135734928+ltmajor42@users.noreply.github.com>
2026-01-01 08:59:53 -0500

    Mark plugin assembly as Windows-only

--- starcitizen/Properties/AssemblyInfo.cs
+++ starcitizen/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Runtime.Versioning;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -33,3 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1.9.0")]
 [assembly: AssemblyFileVersion("1.1.9.0")]
+
+[assembly: SupportedOSPlatform("windows")]
